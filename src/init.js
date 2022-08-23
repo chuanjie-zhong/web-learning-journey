c078dfd0b3bf104f62c969ecffa24df3d8e0c4aa
钟传杰 <1424044350@qq.com>
2022-08-23 22:17:00 +0800

    feat: 模板编译

--- src/init.js
+++ src/init.js
@@ -9,6 +9,20 @@ export function initMixin(Vue) {
         vm.$options = options;
 
         //初始化状态
-        initState(vm)
+        initState(vm)//分割代码
+
+
+
+
+
+        // 如果用户传入了el属性  需要将页面渲染出来
+        // 如果用户传入了el 就要实现挂载流程
+        if(vm.$options.el){
+
+        }
+    }
+
+    Vue.prototype.$mount = function (el) {
+    
     }
 }
